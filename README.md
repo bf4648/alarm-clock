dadad51112dc29e591a749f3e5c69fc84dc44f88
Benjamin Frazier <benjamin.frazier@gmail.com>
2016-02-19 01:42:38 -0500

    updated README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Alarm Clock Notes
+
+## Crontab entry @ 6:45 A.M. M-F created using [cron-tab](http://www.corntab.com/pages/crontab-gui)
+
+```
+45 6 * * 1,2,3,4,5 /Users/fraziert/git/alarm_clock-master/alarm.sh
+```
+
+## Total Possible Sounds so far
+
+```
+    computer.mp3
+    cosmic.mp3
+    cow.mp3
+    digital.mp3
+    farm.mp3
+    forest.mp3
+    ocean.mp3
+    ringer.mp3
+    rooster.mp3
+```
--- alarm.sh
+++ alarm.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 #Refernece http://www.federicopistono.org/blog/tutorial-set-up-an-mp3ogg-alarm-clock-using-linux
 
-PATH=/usr/local/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/bin
+GIT_DIR=$HOME/git
+ALARM_CLOCK_REPO="$GIT_DIR"/alarm-clock
+ALARM_LIST="$ALARM_CLOCK_REPO"/alarmlist.txt
 
-mplayer -loop 0 -shuffle -playlist ~/git/scripts/alarms/mylist.txt
+SOUNDS=(
+    "computer.mp3"
+    "cosmic.mp3"
+    "cow.mp3"
+    "digital.mp3"
+    "farm.mp3"
+    "forest.mp3"
+    "ocean.mp3"
+    "ringer.mp3"
+    "rooster.mp3"
+)
+
+function set_volume_to_max() {
+
+    sudo osascript -e "set Volume 10"
+
+}
+
+function play_alarm() {
+    mplayer -loop 0 -shuffle -playlist "$ALARM_LIST"
+}
+
+set_volume_to_max
+play_alarm
